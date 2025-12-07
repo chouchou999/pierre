453765442523db5aba19fc99a7581cc7156fb0e2
chouchou999 <ckhoury99999@gmail.com>
2025-12-07 23:52:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -895,4 +895,4 @@ def logout():
 if __name__ == '__main__':
     load_allowed_users() 
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
